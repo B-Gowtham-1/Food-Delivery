b8e9f534a7d4b031f95b8da1f4fcd21301b1beb4
B-Gowtham-1 <gowthamsasuke2005@gmail.com>
2025-02-05 10:38:33 +0530

    MODIFICATION IN CART PAGE

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
 const Cart = () => {
+
+  const {cartItems,food_list,removeFromCart} = useContext(StoreContext)
+
   return (
-    <div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-tittle">
+          <p>Items</p>
+          <p>Tittle</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+      </div>
       
     </div>
   )
